90a5d0c6b7d9e551280ac888ace280157accceb1
Amit Yadav <88823371+Tyrant670@users.noreply.github.com>
2022-05-05 18:15:50 +0530

    Update TopDownApproach.cpp

--- DP/Knapsack Related Problems/Knapsack01/TopDownApproach.cpp
+++ DP/Knapsack Related Problems/Knapsack01/TopDownApproach.cpp
@@ -50,7 +50,7 @@ int TopDownknapsack(int wt[],int val[],int w,int n){
 
     for(int i=1;i<n+1;i++){
         for(int j=1;j<w+1;j++){
-            if(wt[i-1]<=w){
+            if(wt[i-1]<=j){
                 dp[i][j] = max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
             }
             else {
